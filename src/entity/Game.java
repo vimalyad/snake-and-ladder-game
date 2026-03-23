a36052ab0f4ee85b5b201acec5bb1d430db740bb
Vimal290704 <vimalyadavkr001@gmail.com>
2026-03-23 21:42:38 +0530

    Added Game and Difficulty entities

--- src/entity/Game.java
+++ src/entity/Game.java
@@ -1,4 +1,30 @@
 package SnakeAndLadderGame.src.entity;
 
+import SnakeAndLadderGame.src.enums.Difficulty;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private final Board board;
+    private final List<Player> players;
+    private final Dice dice;
+    private final Difficulty difficulty;
+    private final List<Player> winners;
+
+    public Game(Board board, Difficulty difficulty, List<Player> players, Dice dice) {
+        this.board = board;
+        this.difficulty = difficulty;
+        this.players = players;
+        this.dice = dice;
+        this.winners = new ArrayList<>();
+    }
+
+    public void addWinner(Player winner) {
+        if (this.winners.contains(winner)) {
+            throw new IllegalArgumentException("Player " + winner + " has already won!");
+        }
+        this.winners.add(winner);
+    }
 }
--- src/enums/Difficulty.java
+++ src/enums/Difficulty.java
@@ -1,4 +1,10 @@
 package SnakeAndLadderGame.src.enums;
 
-public class Difficulty {
+public enum Difficulty {
+    HARD,
+    EASY;
+
+    public static Difficulty getDifficulty(String difficulty) {
+        return Difficulty.valueOf(difficulty.toUpperCase());
+    }
 }
