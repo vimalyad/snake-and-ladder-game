7271cc563783ab4bddc317be6ff8c04278e93d97
Vimal290704 <vimalyadavkr001@gmail.com>
2026-03-23 21:24:44 +0530

    Added board and dice entities

--- src/entity/Board.java
+++ src/entity/Board.java
@@ -1,4 +1,31 @@
 package SnakeAndLadderGame.src.entity;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class Board {
+
+    private final int size;
+    private final Map<Integer, BoardEntity> boardEntityMap;
+
+    public Board(int size) {
+        this.size = size;
+        this.boardEntityMap = new HashMap<>();
+    }
+
+    public void addEntity(int position, BoardEntity entity) {
+        if (position < 0 || position > size) throw new IllegalArgumentException("Position out of range");
+        if (boardEntityMap.containsKey(position))
+            throw new IllegalArgumentException("Entity already exists at position " + position);
+        this.boardEntityMap.put(position, entity);
+    }
+
+    public int totalCells() {
+        return this.size * this.size;
+    }
+
+    public Optional<BoardEntity> getEntity(int position) {
+        return Optional.ofNullable(this.boardEntityMap.get(position));
+    }
 }
--- src/entity/Dice.java
+++ src/entity/Dice.java
@@ -1,4 +1,18 @@
 package SnakeAndLadderGame.src.entity;
 
+import java.util.Random;
+
 public class Dice {
+
+    private final int sides;
+    private final Random rand;
+
+    public Dice(int sides) {
+        this.sides = sides;
+        this.rand = new Random();
+    }
+
+    public int roll() {
+        return this.rand.nextInt(this.sides) + 1;
+    }
 }
